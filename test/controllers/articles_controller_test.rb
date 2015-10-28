483915382a1dbf8d1906f213ebe6dff1c80559d8
CarlosGabaldon <cgabaldon@gmail.com>
2015-10-27 20:05:46 -0700

    basic articles navigation and tests

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,15 +1,23 @@
 require 'test_helper'
 
 class ArticlesControllerTest < ActionController::TestCase
-  test "should get new" do
+  test "#index" do
+    get :index
+    assert assigns(:articles), "should return list of articles"
+  end
+
+  test "#show" do
+    @article = Article.create title: "New Article", text: "New article text"
+    get :show, id: @article
+    assert_template :show
+    assert_equal @article.id, assigns(:article).id
+  end
+
+  test "#new" do
     get :new
     assert_template :new
     assert_response :success
     assert_routing '/articles/new', controller: 'articles', action: 'new'
-  end
-
-  test "should display new form with fields" do
-    get :new
     assert_select 'form'
     assert_select 'label', 'Title'
     assert_select 'input'
@@ -17,5 +25,13 @@ class ArticlesControllerTest < ActionController::TestCase
     assert_select 'textarea'
     assert_select "form input[type=submit][value='Create article']"
   end
+
+  test "#create" do
+    assert_difference 'Article.count' do
+      post :create, article: {title: "Test", text: "Fun test"}
+    end
+    assert_redirected_to article_url(assigns(:article))
+  end
+
 
 end
